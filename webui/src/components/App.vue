aa0d18d4e4d41c206ded83acbb3932380680370a
preigile <diva.artim@gmail.com>
2018-06-02 20:05:09 +0300

    create simple vuejs app with less

---
+++ webui/src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class='container'>
+        Hello {{msg}}!
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+
+        data() {
+            return {
+                msg: 'OKR'
+            }
+        }
+    }
+</script>
