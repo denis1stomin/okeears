36485380b3203d51f54d7ddd75c53841f4fc0d15
Denis <igehuc.dev@gmail.com>
2018-06-21 11:25:01 +0300

    57 fake api server (#10)

---
+++ fake-api-srv/README.md
@@ -0,0 +1,7 @@
+# Fake API server to emulate main OKR API functionality
+
+## JSON Server funny package is used
+https://github.com/typicode/json-server
+
+## Run fake server from terminal
+npm start
--- webui/src/services/subjectservice.js
+++ webui/src/services/subjectservice.js
@@ -5,6 +5,7 @@ class SubjectService {
     constructor(tokenService, httpClientFactory) {
         this.tokenService = tokenService;
         this.httpClient = httpClientFactory.create({
+            //baseURL: 'http://localhost:8001',
             baseURL: 'https://virtserver.swaggerhub.com/denis1stomin/OKRPortal/0.1.0',
             headers: {'Accept': 'application/json'}
         });
@@ -24,16 +25,7 @@ class SubjectService {
         this.httpClient
             //.headers('Authorization', `Bearer ${token}`)
             .get(`/${subjectId || 'me'}/orgtree`)
-            .then(resp => {
-                let data = resp.data;
-                data.push(Object.assign({}, data[0]));
-                data.push(Object.assign({}, data[0]));
-                data[1].id = '351393bd-ebae-4d7e-b755-26b148b700d6';
-                data[1].name = 'Bob Marley';
-                data[2].id = '7003753e-4337-499d-9daf-bca9756a274b';
-                data[2].name = 'Dima Bilan';
-                dataHandler(data);
-            })
+            .then(resp => dataHandler(resp.data))
             .catch(err => errHandler(err));
     }
 }
