fb854687c0dcd02f000bb78deeeea6b1a1e2089f
preigile <diva.artim@gmail.com>
2018-06-29 00:13:03 +0300

    styles for orgtree

--- webui/src/components/OrgTree.vue
+++ webui/src/components/OrgTree.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="org-tree">
-        <span class="org-tree-item" v-for="user in orgtree">
-            {{user.name}}
-            <div class="org-tree-item-links">
+        <div class="org-tree-item" v-for="user in orgtree">
+            <span class="org-tree-item-name">{{user.name}}</span>
+            <span class="org-tree-item-links">
                 View on
-                <a target="_blank" :href="user.aadlink">Portal</a>
-                <a target="_blank" :href="user.o365link">O365</a>
-                <a target="_blank" :href="user.delvelink">Delve</a>
-            </div>
-        </span>
+                <a class="org-tree-item-link" target="_blank" :href="user.aadlink">Portal</a>
+                <a class="org-tree-item-link" target="_blank" :href="user.o365link">O365</a>
+                <a class="org-tree-item-link" target="_blank" :href="user.delvelink">Delve</a>
+            </span>
+        </div>
     </div>
 </template>
 
