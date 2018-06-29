1006c433ff0175256b21175350da916d03da50b2
preigile <diva.artim@gmail.com>
2018-06-30 01:25:01 +0300

    styles for big elements

--- webui/src/components/OKR.vue
+++ webui/src/components/OKR.vue
@@ -1,20 +1,30 @@
 <template>
     <div class="okr">
-        <h3 class="title">OKR Editor</h3>
+        <div class="okr-editor">
+            <h3 class="title">OKR Editor</h3>
 
-        <InputForm ref="newObjForm"
-                   placeholder="Let's create a new objective"
-                   :action="addObjective">
-            <span class="input-icon" @click="addObjective($refs.newObjForm.value)">+</span>
-        </InputForm>
-
-        <div class="objectives" v-for="objective in objectives">
-            <InputForm placeholder=""
-                       :action="editObjective"
-                       :value="objective.statement"
-                       :objid="objective.id">
-                <span class="input-icon" @click="deleteObjective(objective.id)">-</span>
+            <InputForm ref="newObjForm"
+                       placeholder="Let's create a new objective"
+                       :action="addObjective">
+                <span class="input-icon" @click="addObjective($refs.newObjForm.value)">+</span>
             </InputForm>
+
+            <div class="objectives" v-if="objectives.length" v-for="objective in objectives">
+                <InputForm placeholder=""
+                           :action="editObjective"
+                           :value="objective.statement"
+                           :objid="objective.id">
+                    <span class="input-icon" @click="deleteObjective(objective.id)">-</span>
+                </InputForm>
+
+                <div class="key-results" v-for="keyresult in objective.keyresults">
+                    <span>{{keyresult.statement}}</span>
+                </div>
+            </div>
+
+            <div v-if="!objectives.length">
+                <span>You don't have objectives yet. Let's create first! ;)</span>
+            </div>
         </div>
 
         <ChangeLog/>
