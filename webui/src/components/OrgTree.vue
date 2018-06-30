6ae7d516482a540df2f9933a3d505330f09ae289
Denis <igehuc.dev@gmail.com>
2018-06-30 12:33:28 +0300

    added more data

--- webui/src/components/OrgTree.vue
+++ webui/src/components/OrgTree.vue
@@ -2,11 +2,12 @@
     <div class="org-tree">
         <div class="org-tree-item" v-for="user in orgtree">
             <span class="org-tree-item-name">{{user.name}}</span>
+            <span class="org-tree-item-title">{{user.jobTitle}}</span>
             <span class="org-tree-item-links">
-                View on
-                <a class="org-tree-item-link" target="_blank" :href="user.aadlink">Portal</a>
-                <a class="org-tree-item-link" target="_blank" :href="user.o365link">O365</a>
+                View in
                 <a class="org-tree-item-link" target="_blank" :href="user.delvelink">Delve</a>
+                <a class="org-tree-item-link" target="_blank" :href="user.o365link">O365</a>
+                <a class="org-tree-item-link" target="_blank" :href="user.aadlink">Portal</a>
             </span>
         </div>
     </div>
