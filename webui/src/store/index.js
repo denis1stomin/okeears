643ae895fcbae1e4fe8d2bef0cfe6928e1ca0e05
Denis <igehuc.dev@gmail.com>
2018-07-21 00:05:19 +0300

    Made auth modeula as simple js module

--- webui/src/store/index.js
+++ webui/src/store/index.js
@@ -4,7 +4,6 @@ import Vuex from 'vuex';
 import user from './modules/user';
 import okr from './modules/okr';
 import audit from './modules/audit';
-import auth from './modules/auth';
 
 Vue.use(Vuex);
 
@@ -12,7 +11,6 @@ export default new Vuex.Store({
     modules: {
         user,
         okr,
-        audit,
-        auth
+        audit
     }
 });
