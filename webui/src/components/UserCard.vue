8624ce3982c1bf2e17946c00740b222a546f854f
Denis <igehuc.dev@gmail.com>
2018-08-09 12:03:06 +0300

    Removed SAM account from user card and added UPN (#61)

--- webui/src/components/UserCard.vue
+++ webui/src/components/UserCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h4 class="user-card-name">{{user.displayName}}</h4>
+        <h4 class="user-card-name">{{user.givenName}} {{user.surname}}</h4>
         <div class="user-card-img">
             <div class="user-card-photo" v-if="user.photo" :style="{backgroundImage: `url('${user.photo}')`}"></div>
             <UserIcon class="user-card-no-photo" v-if="!user.photo"/>
@@ -8,6 +8,7 @@
         <div class="user-card-details">
             <div class="user-card-item-title">{{user.jobTitle}}</div>
             <div class="user-card-item-title">{{user.officeLocation}}</div>
+            <div class="user-card-item-title">{{user.userPrincipalName}}</div>
             <div class="user-card-item-links">
                 View in
                 <a class="user-card-item-link" target="_blank" rel="noopener noreferrer"
