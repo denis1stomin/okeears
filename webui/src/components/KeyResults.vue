3c00f8da12e9b01031c838b5f155ec422d8ef089
Denis <igehuc.dev@gmail.com>
2018-08-09 12:16:54 +0300

    fixed merge fault - can delete other KR

--- webui/src/components/KeyResults.vue
+++ webui/src/components/KeyResults.vue
@@ -9,12 +9,15 @@
                 <span v-if="canChangeOkr" class="input-icon" @click="deleteKeyresult(objective, keyresult)"><TrashIcon/></span>
             </InputForm>
 
-            <input class="key-result-range" type="range" min="0" max="100" v-if="canChangeOkr"
+            <input class="key-result-range" type="range" min="0" max="100"
+                   v-if="canChangeOkr"
                    v-model.number="keyresult.percent"
                    @blur="editPercent(objective, keyresult)"/>
             <span class="key-result-percents">{{keyresult.percent}}%</span>
 
-            <span class="input-icon" @click="deleteKeyresult(objective, keyresult)"><TrashIcon/></span>
+            <span class="input-icon"
+                  v-if="canChangeOkr"
+                  @click="deleteKeyresult(objective, keyresult)"><TrashIcon/></span>
         </div>
         <InputForm ref="newKRForm"
                    v-if="canChangeOkr"
