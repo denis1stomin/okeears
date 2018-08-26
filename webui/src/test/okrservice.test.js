872d88c93a671c8ab8a78612883968542fbda2de
Sergey Kalitin <sergey.kalitin@gmail.com>
2018-08-26 21:42:01 +0300

    Small reorganization

--- webui/src/test/okrservice.test.js
+++ webui/src/test/okrservice.test.js
@@ -5,4 +5,12 @@ describe('OkrService', () => {
         const service = new OkrService();
         expect(service).toBeDefined();
     })
+
+    it('should return objectives', done => {
+        const service = new OkrService();
+        service.getObjectives('subject', 'user', data => {
+            expect(data).toBeDefined();
+            done();
+        }, error => {});
+    })
 })
