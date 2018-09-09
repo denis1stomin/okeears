0e2f416243272fc6772ce4c6a63d73102ab62193
Sergey Kalitin <sergey.kalitin@gmail.com>
2018-09-09 23:40:25 +0300

    User id as a hash

--- webui/src/services/telemetryservice.js
+++ webui/src/services/telemetryservice.js
@@ -3,12 +3,34 @@ export default class TelemetryService {
     }
 
     setUser(user) {
-        //TODO: Change to ' = hash(user.id)'
-        window.appInsights.context.user.id = user.id;
+        this.sha256(user.id).then(digest => {
+            window.appInsights.context.user.id = digest;
+        })
     }
 
     trackEvent(name, data) {
         window.appInsights.trackEvent(name, data);
+    }
+
+    sha256(value) {
+        let buffer = new TextEncoder("utf-8").encode(value);
+        let telemetry = this;
+        return crypto.subtle.digest("SHA-256", buffer).then(function (hash) {
+          return telemetry.hex(hash);
+        });
+    }
+      
+    hex(buffer) {
+        let hexCodes = [];
+        let view = new DataView(buffer);
+        for (let i = 0; i < view.byteLength; i += 4) {
+            let  value = view.getUint32(i)
+            let stringValue = value.toString(16)
+            let padding = '00000000'
+            let paddedValue = (padding + stringValue).slice(-padding.length)
+            hexCodes.push(paddedValue);
+        }
+        return hexCodes.join("");
     }
 }
 
