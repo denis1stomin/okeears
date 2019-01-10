0b0fcf7a5d28012cbefc9cae60db9d7c99172450
Denis <igehuc.dev@gmail.com>
2019-01-11 00:53:31 +0300

    comment

--- webui/src/services/usercompetitionservice.js
+++ webui/src/services/usercompetitionservice.js
@@ -5,10 +5,14 @@ const ACCESS_TOKEN_RESOURCE = 'https://graph.microsoft.com';
 
 export default class UserCompetitionService {
     getUserLikes(userId, dataHandler, errHandler) {
+
+        // first 24 symbols of md5 of okeears<GUID WITHOUT DASHES>
+        // test storage eacf83e7724ad1dc0ecc42eb
+
         dataHandler({
             userId: userId,
             competitionStatus: "King",
-            numberOfLikes: 100500,
+            numberOfLikes: 8,
             objectiveStars: []
         });
     }
